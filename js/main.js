e1b76fe51c216178bb97cab7c18df3f17068f575
Daniela <daniela.gallardo.reyes@gmail.com>
2021-07-05 15:19:12 +0200

    update

--- js/main.js
+++ js/main.js
@@ -96,24 +96,23 @@
         CountDown
     --------------------*/
     // For demo preview
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    var yyyy = today.getFullYear();
+    // var today = new Date();
+    // var dd = String(today.getDate()).padStart(2, '0');
+    // var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    // var yyyy = today.getFullYear();
 
-    if(mm == 12) {
-        mm = '01';
-        yyyy = yyyy + 1;
-    } else {
-        mm = parseInt(mm) + 1;
-        mm = String(mm).padStart(2, '0');
-    }
-    var timerdate = mm + '/' + dd + '/' + yyyy;
+    // if(mm == 12) {
+    //     mm = '01';
+    //     yyyy = yyyy + 1;
+    // } else {
+    //     mm = parseInt(mm) + 1;
+    //     mm = String(mm).padStart(2, '0');
+    // }
+    // var timerdate = mm + '/' + dd + '/' + yyyy;
     // For demo preview end
-    
 
     // Use this for real timer date
-    /*  var timerdate = "2020/01/01"; */
+    var timerdate = "2021/07/26";
 
 	$("#countdown").countdown(timerdate, function(event) {
         $(this).html(event.strftime("<div class='cd-item'><span>%D</span> <p>Days</p> </div>" + "<div class='cd-item'><span>%H</span> <p>Hrs</p> </div>" + "<div class='cd-item'><span>%M</span> <p>Mins</p> </div>" + "<div class='cd-item'><span>%S</span> <p>Secs</p> </div>"));
